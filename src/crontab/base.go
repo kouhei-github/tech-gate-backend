1b0821f77741e554a359bb483f09c6db02b84cda
nagamatsu-k <nagamatsu-k@dym.jp>
2023-09-30 22:38:43 +0900

    cronジョブの頻度を深夜に変更
    ZennとQiitaのcronジョブは、以前はテスト目的で5秒ごとに実行されていた。しかし、本番環境では、1日に1回だけ実行する方が便利で効率的です。このコミットにより、これらのcronジョブの実行頻度が深夜に変更されました。これはシステムリソースの節約に役立つと同時に、データ収集が最新であることを保証します。

--- src/crontab/base.go
+++ src/crontab/base.go
@@ -7,9 +7,9 @@ import (
 
 func ToStartCron() {
 	jobrunner.Start()
-	jobrunner.Schedule("@every 5s", cron_crawler.Zenn{Url: "https://zenn.dev/api/articles"})
+	jobrunner.Schedule("@midnight", cron_crawler.Zenn{Url: "https://zenn.dev/api/articles"})
 	//
-	jobrunner.Schedule("@every 5s", cron_crawler.Qiita{Url: "http://qiita.com/api/v2/items"})
+	jobrunner.Schedule("@midnight", cron_crawler.Qiita{Url: "http://qiita.com/api/v2/items"})
 	//jobrunner.Schedule("TZ=Asia/Tokyo 30 04 * * * *", cron_crawler.Zenn{Url: "https://zenn.dev/api/articles"})
 
 }
